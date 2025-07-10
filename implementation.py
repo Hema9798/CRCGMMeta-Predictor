cbc8c425713d5c94d9409b31cb6e9d26662aea67
Hema Palanisamy <phemadhawan@gmail.com>
2025-07-10 11:54:18 +0530

    Implementation

---
+++ implementation.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  4 14:14:20 2025
+
+@author: psg
+"""
+
+import pandas as pd
+import numpy as np
+import pickle
+from rdkit import DataStructs
+from rdkit import Chem
+from rdkit.Chem import AllChem
+with open('final_uniprot_fp.pckl', 'rb') as file:
+    df_indexed1=pickle.load(file)
+
+with open('clf3_val_final.pkl', 'rb') as file:
+    clf=pickle.load(file)
+    
+with open('protein_names.pckl', 'rb') as file:
+    name=pickle.load(file)
+    
+EC=pd.read_excel('all_EC_reteived.xlsx')
+
+EC_name=pd.read_excel('EC data.xlsx')
+
+EC_abundance=pd.read_excel('Ec_tax_genus_abun.xlsx')
+
+l=[] 
+for i in range(3001,4410):
+    l.append(i) 
+df_indexed1.columns = l
+
+def CRCGMetaPredict(sm):
+    h=Chem.MolFromSmiles(sm)
+    fpgen=AllChem.GetRDKitFPGenerator()
+    h1=fpgen.GetFingerprint(h)
+    h2=np.zeros((0,),dtype=np.int8)
+    DataStructs.ConvertToNumpyArray(h1,h2)
+    h4=pd.DataFrame(h2)
+    h5=h4.transpose()
+    h5['SMILES']=sm
+    new_df=h5
+    df1=new_df.iloc[:,0:2048]
+    df4=pd.merge(df1, df_indexed1, how='cross')
+    y_pred = clf.predict(df4)
+    y_pred_df=pd.DataFrame(y_pred)
+    res=name.join(y_pred_df)
+    filtered_res = res[res[0] == 1]
+    uni_jn=pd.merge(EC,filtered_res,left_on='Entry',right_on='PROTEIN_ID',how='right')
+    uni_jn1=pd.merge(EC_name,uni_jn,left_on='EC',right_on='EC number',how='right')
+
+    EC_only=uni_jn1[['EC number','Name']].drop_duplicates()
+    uni_jn2=pd.merge(EC_abundance,EC_only,left_on='EC',right_on='EC number',how='right')
+    uni_jn3=uni_jn2[['EC','Name','Genus','abundance']]
+    uni_jn4=uni_jn3.dropna()
+
+    return uni_jn4
+
+
+
+
+
